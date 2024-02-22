2171a39dfd2e7e81485b45e8d4b579ce488b15c2
GarySZ <garysoliz07@gmail.com>
2024-02-22 15:44:48 -0400

    first commit

--- src/api/githubApi.ts
+++ src/api/githubApi.ts
@@ -3,6 +3,6 @@ import axios from "axios";
 export const githubApi = axios.create({
     baseURL: 'https://api.github.com/repos/facebook/react',
     headers: {
-        Authorization: 'Bearer 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'
+        Authorization: 'Bearer ***'
     }
 });
